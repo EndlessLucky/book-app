a9627c5eaa058c7687133cc25536cae4c8336e0d
Olive105 <105.olivesoft@gmail.com>
2021-04-28 07:48:58 -0400

    feat - implement app component file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+
+import { selectBookCollection, selectBooks } from './state/books.selectors';
+import { retrievedBookList, addBook, removeBook } from './state/books.actions';
+import { GoogleBooksService } from './book-list/books.service';
 
 @Component({
   selector: 'book-app-root',
@@ -6,5 +11,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'book-app';
+  books$ = this.store.pipe(select(selectBooks));
+  bookCollection$ = this.store.pipe(select(selectBookCollection)); 
+  onAdd(bookId: any): void {
+    this.store.dispatch(addBook({ bookId }));
+  }
+  onRemove(bookId: any): void {
+    this.store.dispatch(removeBook({ bookId }));
+  }
+  constructor(
+    private booksService: GoogleBooksService,
+    private store: Store
+  ) {}
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngOnInit(): void {
+    this.booksService
+      .getBooks()
+      .subscribe((Book) => this.store.dispatch(retrievedBookList({ Book })));
+  }
 }
